be80cddbd9c06a653464e8da1ee6028b86e6d04a
Mikołaj Bronk <32479479+mikolaj-bronk@users.noreply.github.com>
2018-01-18 16:59:52 +0100

    added missing classes

---
+++ core/src/com/mygdx/game/physics/WallUserData.java
@@ -0,0 +1,24 @@
+package com.mygdx.game.physics;
+
+import com.badlogic.gdx.math.Vector2;
+import com.mygdx.game.enums.UserDataType;
+import com.mygdx.game.utils.Constants;
+
+public class WallUserData extends UserData {
+
+    private Vector2 linearVelocity;
+
+    public WallUserData(float width, float height) {
+        super(width, height);
+        userDataType = UserDataType.WALL;
+    }
+
+    public void setLinearVelocity(Vector2 linearVelocity) {
+        this.linearVelocity = linearVelocity;
+    }
+
+    public Vector2 getLinearVelocity() {
+        return linearVelocity;
+    }
+
+}
