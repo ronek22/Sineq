051ac1ef105235a4f295a168f0a43e1fa97269c4
Mapiatko <marcin.pehnke@o2.pl>
2018-01-20 20:21:07 +0100

    Menu z błędem

--- core/src/com/mygdx/game/screens/GameScreen.java
+++ core/src/com/mygdx/game/screens/GameScreen.java
@@ -2,7 +2,11 @@ package com.mygdx.game.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.game.GameMain;
 import com.mygdx.game.stages.GameStage;
 
@@ -12,12 +16,31 @@ import com.mygdx.game.stages.GameStage;
 
 public class GameScreen implements Screen {
 
+
     private GameStage stage;
     GameMain game;
+    private float Timer;
+    Texture Liczba1;
+    Texture Liczba2;
+    Texture Liczba3;
+    Texture Liczba4;
+    Texture Liczba5;
+    private int Time = 0;
+    private BitmapFont font;
+    private SpriteBatch batch;
 
     public GameScreen(GameMain game) {
         this.game = game;
+        Liczba1 = new Texture("play.png");
+        Liczba1 = new Texture("play.png");
+        Liczba1 = new Texture("play.png");
+        Liczba1 = new Texture("play.png");
+        Liczba1 = new Texture("play.png");
         stage = new GameStage(game);
+
+        batch = new SpriteBatch();
+        font = new BitmapFont();
+        font.setColor(Color.WHITE);
     }
     @Override
     public void render(float delta) {
@@ -26,7 +49,23 @@ public class GameScreen implements Screen {
 
         //Update the stage
         stage.draw();
+        stage.getBatch().begin();
+        Timer += Gdx.graphics.getDeltaTime();
+        if(Timer > 1){
+            Time++;
+            System.out.println(Time);
+            Timer=0;
+
+        }
+        batch.begin();
+        font.draw(batch, String.valueOf(Time), 250, 400); //you can change the position as you like
+        batch.end();
+
+        stage.getBatch().end();
+
+        //stage.draw(Time, 50, 200);
         stage.act(delta);
+
     }
 
     @Override
@@ -61,3 +100,4 @@ public class GameScreen implements Screen {
 
     }
 }
+
