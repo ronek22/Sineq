abd570ced840d08ec8bc9ef3a132c2c4cd5fc022
Jakub Ronkiewicz <jronek3010@gmail.com>
2018-01-20 23:06:45 +0100

    Android build fix

--- android/src/com/mygdx/game/AndroidLauncher.java
+++ android/src/com/mygdx/game/AndroidLauncher.java
@@ -5,12 +5,18 @@ import android.os.Bundle;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.mygdx.game.GameMain;
+import com.mygdx.game.utils.GameEventListener;
 
-public class AndroidLauncher extends AndroidApplication {
+public class AndroidLauncher extends AndroidApplication implements GameEventListener {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new GameMain(), config);
+		initialize(new GameMain(this), config);
+	}
+
+	@Override
+	public void submitScore(int score) {
+
 	}
 }
